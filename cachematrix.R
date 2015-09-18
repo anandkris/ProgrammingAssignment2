bceba5d34473ad06c0b9918b5edc6714b1b080dc
anandkris <anandkris68@gmail.com>
2015-09-17 22:00:13 -0700

    More comments added to both functions
    This makes the working explicit

--- cachematrix.R
+++ cachematrix.R
@@ -2,12 +2,14 @@
 ## functions do
 
 ## This function creates a list of 4 objects which contains the original matrix and 
-## a placeholder for he inverse. This needs to be used in conjunction with 
+## a placeholder for the inverse. This needs to be used in conjunction with 
 ## cacheSolve function
+## Input has to be a square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
-    set <- function(y) {
+    set <- function(y) { 
+        ##By using <<- operator, we can pass value from the calling function
         x <<- y
         m <<- NULL
     }
@@ -24,14 +26,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Computes the inverse of a matrix. If the inverse is already present, it will 
 ## use that. Otherwise, it will create a new one. For large matrices this saves
 ## significant amount of time
+## For computing the inverse, the matrix has to be a square matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     m <- x$getinv()
-    if(!is.null(m)) {
+    if(!is.null(m)) { 
+        #Tests if the inverse is already computed, if so use cached data
         message("getting cached data")
         return(m)
     }
+    ## Otherwise, get the original matrix and compute the inverse. Store it in $setinv
     data <- x$get()
     m <- solve(data, ...)
     x$setinv(m)
